6a9a4abc87d7652d9b0aefa882ae0b71f237d941
{linjiale} <{674481877@qq.com}>
2018-03-04 00:48:26 +0800

    taskbean

--- src/main/java/com/yqbd/beans/TaskBean.java
+++ src/main/java/com/yqbd/beans/TaskBean.java
@@ -14,25 +14,57 @@ import java.util.List;
 @NoArgsConstructor
 public class TaskBean implements Serializable {
     private Integer taskId;
-    private Integer userId;
+
     private Integer companyId;
+
+    private Integer userId;
+
+    private String classification;
+
     private String taskTitle;
-    private String taskDescription;
-    private String taskAddress;
-    private Double pay;
+
     private Integer taskStatus;
+
+    private Double pay;
+
     private Long publishTime;
-    private Long completeTime;
-    private Long startTime;
-    private Integer maxPeopleNumber;
+
     private Long deadline;
 
+    private Long startTime;
+
+    private Long completeTime;
+
+    private Integer signUpPeopleNumber;
+
+    private Integer currentPeopleNumber;
+
+    private Integer maxPeopleNumber;
+
     private String simpleDrawingAddress;
+
+    private Integer groupId;
+
+    private String province;
+
+    private String city;
+
+    private String district;
+
+    private String taskAddress;
+
+    private String taskDescription;
+
+    private String primaryWork;
+
+    private String otherCompany;
+
+    private String primaryContact;
+
+    private String remark;
+
     private Integer isGroup;
     private List<TypeBean> typeBeans;
     private String name;
-    private String primaryWork;
-    private String otherCompany;
-    private String primaryContact;
-    private String remark;
+
 }
